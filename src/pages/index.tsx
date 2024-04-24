0ed969badefea1cb364dd49678e0224fb480c2ff
Latte39777 <s1f102400549@iniad.org>
2024-04-24 17:06:19 +0900

    短縮でけた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,22 +18,22 @@ const directions = [
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 1],
+    // [0, 0, 0, 0, 0, 0, 2, 1],
+    // [0, 0, 0, 0, 0, 1, 2, 1],
+    // [0, 0, 0, 0, 2, 1, 2, 1],
+    // [0, 0, 0, 1, 2, 1, 2, 1],
+    // [0, 0, 2, 1, 2, 1, 2, 1],
+    // [0, 1, 1, 2, 1, 2, 1, 2],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 2, 1],
-    [0, 0, 0, 0, 0, 1, 2, 1],
-    [0, 0, 0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 1, 2, 1, 2, 1],
-    [0, 0, 2, 1, 2, 1, 2, 1],
-    [0, 1, 1, 2, 1, 2, 1, 2],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 2, 1, 0, 0, 0],
-    // [0, 0, 0, 1, 2, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
   const clickHandler = (x: number, y: number) => {
@@ -41,126 +41,107 @@ const Home = () => {
     console.log(turnColor);
     const newBoard = structuredClone(board);
 
-    function checkSameColor(x: number, y: number) {
-      console.log(1);
+    // function checkSameColor(x: number, y: number) {
+    for (const direction of directions) {
+      const dx = direction[0]; //0;
+      const dy = direction[1]; //1;
+      //const newX = x + dx;
+      //const newY = y + dy;
 
-      for (const direction of directions) {
-        console.log(2);
-        const dx = 0;
-        // direction[0];
-        const dy = 1;
-        // direction[1];
-        //const newX = x + dx;
-        //const newY = y + dy;
-
-        for (let a = 1; a <= 7; a++) {
-          if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
-            if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
-              if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
-                if (isNewValid(x + dx * 4, y + dy * 4) && isAnotherColor(x + dx * 4, y + dy * 4)) {
-                  if (
-                    isNewValid(x + dx * 5, y + dy * 5) &&
-                    isAnotherColor(x + dx * 5, y + dy * 5)
-                  ) {
-                    if (
-                      isNewValid(x + dx * 6, y + dy * 6) &&
-                      isAnotherColor(x + dx * 6, y + dy * 6)
-                    ) {
-                      if (
-                        isNewValid(x + dx * 7, y + dy * 7) &&
-                        isSameColor(x + dx * 7, y + dy * 7)
-                      ) {
-                        newBoard[y][x] = turnColor;
-                        newBoard[y + dy * 1][x + dx * 1] = turnColor;
-                        newBoard[y + dy * 2][x + dx * 2] = turnColor;
-                        newBoard[y + dy * 3][x + dx * 3] = turnColor;
-                        newBoard[y + dy * 4][x + dx * 4] = turnColor;
-                        newBoard[y + dy * 5][x + dx * 5] = turnColor;
-                        newBoard[y + dy * 6][x + dx * 6] = turnColor;
-                        setTurnColor(3 - turnColor);
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-          if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
-            if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
-              if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
-                if (isNewValid(x + dx * 4, y + dy * 4) && isAnotherColor(x + dx * 4, y + dy * 4)) {
-                  if (
-                    isNewValid(x + dx * 5, y + dy * 5) &&
-                    isAnotherColor(x + dx * 5, y + dy * 5)
-                  ) {
-                    if (isNewValid(x + dx * 6, y + dy * 6) && isSameColor(x + dx * 6, y + dy * 6)) {
-                      newBoard[y][x] = turnColor;
-                      newBoard[y + dy * 1][x + dx * 1] = turnColor;
-                      newBoard[y + dy * 2][x + dx * 2] = turnColor;
-                      newBoard[y + dy * 3][x + dx * 3] = turnColor;
-                      newBoard[y + dy * 4][x + dx * 4] = turnColor;
-                      newBoard[y + dy * 5][x + dx * 5] = turnColor;
-                      setTurnColor(3 - turnColor);
-                    }
-                  }
-                }
-              }
-            }
-          }
-
-          if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
-            if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
-              if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
-                if (isNewValid(x + dx * 4, y + dy * 4) && isAnotherColor(x + dx * 4, y + dy * 4)) {
-                  if (isNewValid(x + dx * 5, y + dy * 5) && isSameColor(x + dx * 5, y + dy * 5)) {
-                    newBoard[y][x] = turnColor;
-                    newBoard[y + dy * 1][x + dx * 1] = turnColor;
-                    newBoard[y + dy * 2][x + dx * 2] = turnColor;
-                    newBoard[y + dy * 3][x + dx * 3] = turnColor;
-                    newBoard[y + dy * 4][x + dx * 4] = turnColor;
-                    setTurnColor(3 - turnColor);
-                  }
-                }
-              }
-            }
-          }
-
-          if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
-            if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
-              if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
-                if (isNewValid(x + dx * 4, y + dy * 4) && isSameColor(x + dx * 4, y + dy * 4)) {
-                  newBoard[y][x] = turnColor;
-                  newBoard[y + dy * 1][x + dx * 1] = turnColor;
-                  newBoard[y + dy * 2][x + dx * 2] = turnColor;
-                  newBoard[y + dy * 3][x + dx * 3] = turnColor;
-                  setTurnColor(3 - turnColor);
-                }
-              }
-            }
-          }
-          if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
-            if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
-              if (isNewValid(x + dx * 3, y + dy * 3) && isSameColor(x + dx * 3, y + dy * 3)) {
-                newBoard[y][x] = turnColor;
-                newBoard[y + dy * 1][x + dx * 1] = turnColor;
-                newBoard[y + dy * 2][x + dx * 2] = turnColor;
-                setTurnColor(3 - turnColor);
-              }
-            }
-          }
-          if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
-            if (isNewValid(x + dx * 2, y + dy * 2) && isSameColor(x + dx * 2, y + dy * 2)) {
+      for (let a: number = 1; a <= 6; a++) {
+        if (isNewValid(x + dx * a, y + dy * a) && isAnotherColor(x + dx * a, y + dy * a)) {
+          console.log('あ');
+          if (
+            isNewValid(x + dx * (a + 1), y + dy * (a + 1)) &&
+            isSameColor(x + dx * (a + 1), y + dy * (a + 1))
+          ) {
+            console.log('いい');
+            for (let b = 1; b <= a; b++) {
+              console.log('ううう');
               newBoard[y][x] = turnColor;
-              newBoard[y + dy * 1][x + dx * 1] = turnColor;
-
+              newBoard[y + dy * b][x + dx * b] = turnColor;
+              console.log('ええええ');
               setTurnColor(3 - turnColor);
-              // newBoard[newY][newX] = turnColor;
-              // foundCoordinates.push([newX, newY]);
             }
           }
         }
+        if (isNewValid(x + dx * a, y + dy * a) && isZeroColor(x + dx * a, y + dy * a)) {
+          break;
+        }
       }
+
+      // if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
+      //   if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
+      //     if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
+      //       if (isNewValid(x + dx * 4, y + dy * 4) && isAnotherColor(x + dx * 4, y + dy * 4)) {
+      //         if (isNewValid(x + dx * 5, y + dy * 5) && isAnotherColor(x + dx * 5, y + dy * 5)) {
+      //           if (isNewValid(x + dx * 6, y + dy * 6) && isSameColor(x + dx * 6, y + dy * 6)) {
+      //             newBoard[y][x] = turnColor;
+      //             newBoard[y + dy * 1][x + dx * 1] = turnColor;
+      //             newBoard[y + dy * 2][x + dx * 2] = turnColor;
+      //             newBoard[y + dy * 3][x + dx * 3] = turnColor;
+      //             newBoard[y + dy * 4][x + dx * 4] = turnColor;
+      //             newBoard[y + dy * 5][x + dx * 5] = turnColor;
+      //             setTurnColor(3 - turnColor);
+      //           }
+      //         }
+      //       }
+      //     }
+      //   }
+      // }
+
+      // if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
+      //   if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
+      //     if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
+      //       if (isNewValid(x + dx * 4, y + dy * 4) && isAnotherColor(x + dx * 4, y + dy * 4)) {
+      //         if (isNewValid(x + dx * 5, y + dy * 5) && isSameColor(x + dx * 5, y + dy * 5)) {
+      //           newBoard[y][x] = turnColor;
+      //           newBoard[y + dy * 1][x + dx * 1] = turnColor;
+      //           newBoard[y + dy * 2][x + dx * 2] = turnColor;
+      //           newBoard[y + dy * 3][x + dx * 3] = turnColor;
+      //           newBoard[y + dy * 4][x + dx * 4] = turnColor;
+      //           setTurnColor(3 - turnColor);
+      //         }
+      //       }
+      //     }
+      //   }
+      // }
+
+      // if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
+      //   if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
+      //     if (isNewValid(x + dx * 3, y + dy * 3) && isAnotherColor(x + dx * 3, y + dy * 3)) {
+      //       if (isNewValid(x + dx * 4, y + dy * 4) && isSameColor(x + dx * 4, y + dy * 4)) {
+      //         newBoard[y][x] = turnColor;
+      //         newBoard[y + dy * 1][x + dx * 1] = turnColor;
+      //         newBoard[y + dy * 2][x + dx * 2] = turnColor;
+      //         newBoard[y + dy * 3][x + dx * 3] = turnColor;
+      //         setTurnColor(3 - turnColor);
+      //       }
+      //     }
+      //   }
+      // }
+      // if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
+      //   if (isNewValid(x + dx * 2, y + dy * 2) && isAnotherColor(x + dx * 2, y + dy * 2)) {
+      //     if (isNewValid(x + dx * 3, y + dy * 3) && isSameColor(x + dx * 3, y + dy * 3)) {
+      //       newBoard[y][x] = turnColor;
+      //       newBoard[y + dy * 1][x + dx * 1] = turnColor;
+      //       newBoard[y + dy * 2][x + dx * 2] = turnColor;
+      //       setTurnColor(3 - turnColor);
+      //     }
+      //   }
+      // }
+      // if (isNewValid(x + dx * 1, y + dy * 1) && isAnotherColor(x + dx * 1, y + dy * 1)) {
+      //   if (isNewValid(x + dx * 2, y + dy * 2) && isSameColor(x + dx * 2, y + dy * 2)) {
+      //     newBoard[y][x] = turnColor;
+      //     newBoard[y + dy * 1][x + dx * 1] = turnColor;
+
+      //     setTurnColor(3 - turnColor);
+      //     // newBoard[newY][newX] = turnColor;
+      //     // foundCoordinates.push([newX, newY]);
+      //   }
+      // }
     }
+    // }
 
     function isAnotherColor(x: number, y: number) {
       // console.log('isAnotherColor', x, y, turnColor, board[x][y]);
@@ -169,7 +150,10 @@ const Home = () => {
     function isSameColor(x: number, y: number) {
       return board[y][x] === turnColor;
     }
-    checkSameColor(x, y);
+    function isZeroColor(x: number, y: number) {
+      return board[y][x] === 0;
+    }
+    // checkSameColor(x, y);
 
     // if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
     //   newBoard[y][x] = turnColor;
@@ -188,6 +172,12 @@ const Home = () => {
                 <div
                   className={styles.stone}
                   style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+              {color === 0 && (
+                <div
+                  className={styles.circle}
+                  style={{ background: color === 0 ? '#0000ff' : '#0000000' }}
                 />
               )}
             </div>
