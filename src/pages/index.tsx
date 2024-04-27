00a145dcd157a42812cf11921ce2c4a8ed5fefb7
Latte39777 <s1f102400549@iniad.org>
2024-04-27 20:01:26 +0900

    表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ const Home = () => {
     // [0, 0, 0, 0, 0, 0, 0, 0],
     // [0, 0, 0, 0, 0, 0, 0, 0],
     // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 1, 2, 0, 0, 0],
     // [0, 0, 0, 2, 1, 0, 0, 0],
+    // [0, 0, 0, 1, 2, 0, 0, 0],
     // [0, 0, 0, 0, 0, 0, 0, 0],
     // [0, 0, 0, 0, 0, 0, 0, 0],
     // [0, 0, 0, 0, 0, 0, 0, 0],
@@ -47,13 +47,18 @@ const Home = () => {
   function isZeroColor(x: number, y: number) {
     return board[y][x] === 0;
   }
+  function Represent(x: number, y: number) {
+    return board[y][x] === 3;
+  }
+
+  const newBoard = structuredClone(board);
 
   const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
-    console.log(turnColor);
-    const newBoard = structuredClone(board);
+    // console.log(x, y);
+    // console.log(turnColor);
+    // const newBoard = structuredClone(board);
 
-    if (isNewValid(x, y) && isZeroColor(x, y)) {
+    if ((isNewValid(x, y) && isZeroColor(x, y)) || Represent(x, y)) {
       for (const direction of directions) {
         const dx = direction[0]; //0;
         const dy = direction[1]; //1;
@@ -64,9 +69,12 @@ const Home = () => {
               isSameColor(x + dx * (a + 1), y + dy * (a + 1))
             ) {
               for (let b = 1; b <= a; b++) {
+                console.log('ううう');
                 newBoard[y][x] = turnColor;
                 newBoard[y + dy * b][x + dx * b] = turnColor;
                 setTurnColor(3 - turnColor);
+                // setBoard(newBoard);
+                setBoard(structuredClone(board));
               }
               break;
             }
@@ -79,48 +87,60 @@ const Home = () => {
           if (isNewValid(x + dx * a, y + dy * a) && isZeroColor(x + dx * a, y + dy * a)) {
             break;
           }
+
+          if (isNewValid(x + dx * a, y + dy * a) && Represent(x + dx * a, y + dy * a)) {
+            break;
+          }
         }
       }
     }
-    setBoard(newBoard);
   };
-
 
   const getPutPlace = (x: number, y: number) => {
     console.log(x, y);
     console.log(turnColor);
-    const newBoard = structuredClone(board);
-    if (isNewValid(x, y) && isZeroColor(x, y)) {
-      for (const direction of directions) {
-        const dx = direction[0]; //0;
-        const dy = direction[1]; //1;
-        for (let a: number = 1; a <= 6; a++) {
-          if (isNewValid(x + dx * a, y + dy * a) && isAnotherColor(x + dx * a, y + dy * a)) {
-            console.log('あ');
-            if (
-              isNewValid(x + dx * (a + 1), y + dy * (a + 1)) &&
-              isSameColor(x + dx * (a + 1), y + dy * (a + 1))
-            ) {
-              console.log('いい');
-              for (let b = 1; b <= a; b++) {
-                console.log("ううう");
+
+    // const newBoard = structuredClone(board);
+    // = JSON.parse(JSON.stringify(board));
+
+    for (let c = 0; c <= 7; c++) {
+      for (let d = 0; d <= 7; d++) {
+        if (isNewValid(c, d) && (isZeroColor(c, d) || Represent(c, d))) {
+          for (const direction of directions) {
+            const dx = direction[0]; //0;
+            const dy = direction[1]; //1;
+            for (let a: number = 1; a <= 6; a++) {
+              if (isNewValid(c + dx * a, d + dy * a) && isAnotherColor(c + dx * a, d + dy * a)) {
+                console.log(x, y, 'a', c, d);
+                if (
+                  isNewValid(c + dx * (a + 1), d + dy * (a + 1)) &&
+                  isSameColor(c + dx * (a + 1), d + dy * (a + 1))
+                ) {
+                  console.log(x, y, 'bb', c, d);
+                  // newBoard[y][x] = turnColor;
+                  // setTurnColor(3 - turnColor);
+                  newBoard[d][c] = 3;
+                  // setBoard(structuredClone(board));
+                  setBoard(newBoard);
+                }
               }
-              break;
+
+              if (isNewValid(c + dx * a, d + dy * a) && isSameColor(c + dx * a, d + dy * a)) {
+                break;
+              }
+
+              if (isNewValid(c + dx * a, d + dy * a) && isZeroColor(c + dx * a, d + dy * a)) {
+                break;
+              }
+
+              if (isNewValid(c + dx * a, d + dy * a) && Represent(c + dx * a, d + dy * a)) {
+                break;
+              }
             }
-          }
-
-          if (isNewValid(x + dx * a, y + dy * a) && isSameColor(x + dx * a, y + dy * a)) {
-            break;
-          }
-
-          if (isNewValid(x + dx * a, y + dy * a) && isZeroColor(x + dx * a, y + dy * a)) {
-            break;
           }
         }
       }
     }
-    newBoard[y][x] = 3;
-    setBoard(newBoard);
   };
 
   return (
@@ -128,16 +148,29 @@ const Home = () => {
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+            <div
+              className={styles.cell}
+              key={`${x}-${y}`}
+              onClick={() => {
+                clickHandler(x, y);
+                getPutPlace(x, y);
+              }}
+            >
+              {/* {color !== 0 && color !== 3 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )} */}
+              {/* <div className={styles.cell} key={`${x}-${y}`} onClick={() => getPutPlace(x, y)}> */}
               {color !== 0 && color !== 3 && (
                 <div
                   className={styles.stone}
                   style={{ background: color === 1 ? '#000' : '#fff' }}
                 />
               )}
-            </div>
-            <div className={styles.cell} key={`${x}-${y}`} inplace={() => getPutPlace(x, y)}>
               {color === 3 && <div className={styles.circle} style={{ background: '#0000ff' }} />}
+              {/* </div> */}
             </div>
           )),
         )}
