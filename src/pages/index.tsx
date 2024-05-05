b95d1a718cc82d414bfafbf4982ef3a53a1c1afd
Latte39777 <s1f102400549@iniad.org>
2024-05-05 23:14:51 +0900

    ほぼ完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './index.module.css';
 import { get } from 'http';
 
@@ -20,23 +20,26 @@ const directions = [
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
+    // [1, 2, 0, 0, 0, 0, 0, 0],
+    // [2, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 2, 1],
-    [0, 0, 0, 0, 0, 1, 2, 1],
-    [0, 0, 0, 0, 2, 1, 2, 1],
-    [0, 0, 0, 1, 2, 1, 2, 1],
-    [0, 0, 2, 1, 2, 1, 2, 1],
-    [0, 1, 1, 2, 1, 2, 1, 2],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 2, 1, 0, 0, 0],
-    // [0, 0, 0, 1, 2, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const [blackStones, setBlackStones] = useState(0);
+  const [whiteStones, setWhiteStones] = useState(0);
 
   function isAnotherColor(x: number, y: number) {
     return board[y][x] === 3 - turnColor;
@@ -53,14 +56,20 @@ const Home = () => {
 
   const newBoard = structuredClone(board);
 
+  useEffect(() => {
+    getPutPlace();
+  }, [turnColor]);
+
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     console.log(turnColor);
 
-    if ((isNewValid(x, y) && isZeroColor(x, y)) || Represent(x, y)) {
+    if (isNewValid(x, y) && Represent(x, y)) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
       for (const direction of directions) {
-        const dx = direction[0]; //0;
-        const dy = direction[1]; //1;
+        const dx = direction[0];
+        const dy = direction[1];
         for (let a: number = 1; a <= 6; a++) {
           if (isNewValid(x + dx * a, y + dy * a) && isAnotherColor(x + dx * a, y + dy * a)) {
             if (
@@ -69,67 +78,11 @@ const Home = () => {
             ) {
               for (let b = 1; b <= a; b++) {
                 newBoard[y + dy * b][x + dx * b] = turnColor;
-                // newBoard[y][x] = turnColor;
-                setTurnColor(3 - turnColor);
-                // setBoard(newBoard);
-                setBoard(structuredClone(board));
               }
               break;
             }
-          }
-
-          if (isNewValid(x + dx * a, y + dy * a) && isSameColor(x + dx * a, y + dy * a)) {
+          } else {
             break;
-          }
-
-          if (isNewValid(x + dx * a, y + dy * a) && isZeroColor(x + dx * a, y + dy * a)) {
-            break;
-          }
-
-          if (isNewValid(x + dx * a, y + dy * a) && Represent(x + dx * a, y + dy * a)) {
-            break;
-          }
-        }
-      }
-    }
-  };
-
-  const getPutPlace = (x: number, y: number) => {
-    // = JSON.parse(JSON.stringify(board));
-
-    for (let c = 0; c <= 7; c++) {
-      for (let d = 0; d <= 7; d++) {
-        if (isNewValid(c, d) && (isZeroColor(c, d) || Represent(c, d))) {
-          for (const direction of directions) {
-            const dx = direction[0]; //0;
-            const dy = direction[1]; //1;
-            for (let a: number = 1; a <= 6; a++) {
-              if (isNewValid(c + dx * a, d + dy * a) && isAnotherColor(c + dx * a, d + dy * a)) {
-                if (
-                  isNewValid(c + dx * (a + 1), d + dy * (a + 1)) &&
-                  isSameColor(c + dx * (a + 1), d + dy * (a + 1))
-                ) {
-                  newBoard[d][c] = 3;
-                  if (isNewValid(c, d) === isNewValid(x, y)) {
-                    // setBoard(structuredClone(board));
-
-                    newBoard[y][x] = turnColor;
-                  }
-                }
-              }
-
-              if (isNewValid(c + dx * a, d + dy * a) && isSameColor(c + dx * a, d + dy * a)) {
-                break;
-              }
-
-              if (isNewValid(c + dx * a, d + dy * a) && isZeroColor(c + dx * a, d + dy * a)) {
-                break;
-              }
-
-              if (isNewValid(c + dx * a, d + dy * a) && Represent(c + dx * a, d + dy * a)) {
-                break;
-              }
-            }
           }
         }
       }
@@ -137,34 +90,81 @@ const Home = () => {
     setBoard(newBoard);
   };
 
+  const getPutPlace = () => {
+    let candidatecount = 0;
+    for (let c = 0; c <= 7; c++) {
+      for (let d = 0; d <= 7; d++) {
+        if (isZeroColor(c, d) || Represent(c, d)) {
+          newBoard[d][c] = 0;
+          for (const direction of directions) {
+            const dx = direction[0];
+            const dy = direction[1];
+            for (let a: number = 1; a <= 6; a++) {
+              if (isNewValid(c + dx * a, d + dy * a) && isAnotherColor(c + dx * a, d + dy * a)) {
+                if (
+                  isNewValid(c + dx * (a + 1), d + dy * (a + 1)) &&
+                  isSameColor(c + dx * (a + 1), d + dy * (a + 1))
+                ) {
+                  newBoard[d][c] = 3;
+                  candidatecount++;
+                  break;
+                }
+              } else {
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    let blackCount = 0;
+    let whiteCount = 0;
+    board.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell === 1) blackCount++;
+        if (cell === 2) whiteCount++;
+      });
+    });
+    setBlackStones(blackCount);
+    setWhiteStones(whiteCount);
+
+    // if (candidatecount === 0 && turnColor !== 3 - turnColor) {
+    //   setTurnColor(3 - turnColor);
+    // }
+    setBoard(newBoard);
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.mainContainer}>
+      <div className={`${styles.setcontainer} ${styles.alignCenter}`}>
+        <div
+          className={`${styles.yourturn} ${styles.textLarge} ${styles.textBold} ${styles.textCenter}`}
+        >
+          Turn: {turnColor === 1 ? 'Black' : 'White'}
+        </div>
+        <div className={`${styles.numcolor} ${styles.textLarge} ${styles.textCenter}`}>
+          <div className={styles.textBold}>Black Stones: {blackStones}</div>
+          <div className={styles.textBold}>White Stones: {whiteStones}</div>
+        </div>
+      </div>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
             <div
-              className={styles.cell}
+              className={`${styles.cell} ${styles.alignCenter}`}
               key={`${x}-${y}`}
               onClick={() => {
                 clickHandler(x, y);
-                getPutPlace(x, y);
               }}
             >
-              {/* {color !== 0 && color !== 3 && (
-                <div
-                  className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : '#fff' }}
-                />
-              )} */}
-              {/* <div className={styles.cell} key={`${x}-${y}`} onClick={() => getPutPlace(x, y)}> */}
               {color !== 0 && color !== 3 && (
                 <div
                   className={styles.stone}
                   style={{ background: color === 1 ? '#000' : '#fff' }}
                 />
               )}
-              {color === 3 && <div className={styles.circle} style={{ background: '#0000ff' }} />}
-              {/* </div> */}
+              {color === 3 && <div className={styles.circle} style={{ background: '#0000FF' }} />}
             </div>
           )),
         )}
