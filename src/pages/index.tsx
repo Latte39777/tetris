96e6b7d6b7af4442dc355acbf22bb377d4faf48f
Latte39777 <s1f102400549@iniad.org>
2024-06-05 11:02:30 +0900

    ミノ消し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,146 +1,197 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import styles from './index.module.css';
 
-const directions = [
-  [0, 1],
-  [1, 1],
-  [1, 0],
-  [1, -1],
-  [0, -1],
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-];
+function number1() {
+  const iMino = [[1], [1], [1], [1]];
+  return iMino;
+}
+function number2() {
+  const oMino = [
+    [2, 2],
+    [2, 2],
+  ];
+  return oMino;
+}
 
-const reversePlace = (x: number, y: number, board: number[][], turnColor: number) => {
-  board[y][x] = turnColor;
-  for (const direction of directions) {
-    for (let a: number = 1; a <= 6; a++) {
-      if (
-        board[y + direction[1] * a] !== undefined &&
-        board[y + direction[1] * a][x + direction[0] * a] === 3 - turnColor
-      ) {
-        if (
-          board[y + direction[1] * (a + 1)] !== undefined &&
-          board[y + direction[1] * (a + 1)][x + direction[0] * (a + 1)] === turnColor
-        ) {
-          for (let b: number = 1; b <= a; b++) {
-            board[y + direction[1] * b][x + direction[0] * b] = turnColor;
-          }
-        }
-      } else {
-        break;
-      }
+function number3() {
+  const sMino = [
+    [0, 3, 3],
+    [3, 3, 0],
+  ];
+  return sMino;
+}
+
+function number4() {
+  const zMino = [
+    [4, 4, 0],
+    [0, 4, 4],
+  ];
+  return zMino;
+}
+
+function number5() {
+  const lMino = [
+    [0, 0, 5],
+    [5, 5, 5],
+  ];
+  return lMino;
+}
+
+function number6() {
+  const jMino = [
+    [6, 0, 0],
+    [6, 6, 6],
+  ];
+  return jMino;
+}
+
+function number7() {
+  const tMino = [
+    [0, 7, 0],
+    [7, 7, 7],
+  ];
+  return tMino;
+}
+
+function getRandomNumber() {
+  const minoNumber = Math.floor(Math.random() * 7) + 1;
+  return minoNumber;
+}
+
+//ミノ選択
+const minoOpt = () => {
+  const minoList: number[] = [];
+  while (minoList.length < 7) {
+    const minoNumber = getRandomNumber();
+    if (!minoList.includes(minoNumber)) {
+      minoList.push(minoNumber);
     }
   }
-  return board;
+  console.log(minoList);
+  return minoList;
 };
 
-const getPutPlace = (board: number[][], turnColor: number, passCount: number) => {
-  let candidatecount = 0;
-  for (let c = 0; c <= 7; c++) {
-    for (let d = 0; d <= 7; d++) {
-      if (board[d][c] === 0 || board[d][c] === 3) {
-        board[d][c] = 0;
-        for (const direction of directions) {
-          for (let a: number = 1; a <= 6; a++) {
-            if (
-              board[d + direction[1] * a] !== undefined &&
-              board[d + direction[1] * a][c + direction[0] * a] === turnColor
-            ) {
-              if (
-                board[d + direction[1] * (a + 1)] !== undefined &&
-                board[d + direction[1] * (a + 1)][c + direction[0] * (a + 1)] === 3 - turnColor
-              ) {
-                candidatecount++;
-                board[d][c] = 3;
-              }
-            } else {
-              break;
-            }
-          }
-        }
+//ミノオプトからミノ取得
+//最初の場所のみ
+const minoAppear = (board: number[][], minoNumber: number) => {
+  if(minoNumber === )
+};
+
+const minoMovement = (board: number[][], minoNunber: number) => {
+  for (let a = 0; a <= 10; a++) {
+    for (let b = 0; b <= 20; b++) {
+      //右に動かす
+      if (board[b][a] === minoNunber && board[b + 1][a] !== undefined) {
+        board[b + 1][a] = minoNunber;
+      }
+      //左に動かす
+      if (board[b][a] === minoNunber && board[b - 1][a] !== undefined) {
+        board[b - 1][a] = minoNunber;
+      }
+      //下に動かす
+      if (board[b][a] === minoNunber && board[b][a - 1] !== undefined) {
+        board[b][a - 1] = minoNunber;
       }
     }
   }
-  if (candidatecount === 0 && passCount <= 2) {
-    getPutPlace(board, 3 - turnColor, 1 + passCount);
-    console.log('AA', passCount);
+};
+
+//揃ったLINE消す
+const delateLine = (board: number[][]) => {
+  for (let a = 0; a < 20; a++) {
+    let deletePossibiltty = 0;
+    for (let row = 0; row < 10; row++) {
+      if (board[a][row] !== 0) {
+        deletePossibiltty++;
+      }
+    }
+    // console.log('a', deletePossibiltty);
+    if (deletePossibiltty === 10) {
+      board.splice(a, 1);
+      board.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
   }
   return board;
 };
 
 const Home = () => {
-  const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
-    // [0, 0, 0, 0, 0, 3, 2, 1],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 3, 2, 1],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 3, 2, 1],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    // [0, 0, 0, 0, 0, 3, 2, 1],
-    // [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 3, 0, 0, 0, 0],
-    [0, 0, 3, 2, 1, 0, 0, 0],
-    [0, 0, 0, 1, 2, 3, 0, 0],
-    [0, 0, 0, 0, 3, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 2, 2, 0, 3, 3, 4, 4, 0, 0],
+    [1, 2, 2, 3, 3, 1, 1, 4, 4, 1],
+    [1, 0, 5, 0, 0, 6, 0, 7, 0, 0],
+    [1, 0, 5, 0, 0, 6, 7, 7, 7, 0],
+    [0, 0, 5, 5, 6, 6, 0, 0, 0, 0],
   ]);
 
-  const passCount = 0;
+  const downCell = () => {
+    console.log('down');
+  };
+
   const clickHandler = (x: number, y: number) => {
-    if (board[y][x] === 3) {
-      const newBoard = structuredClone(board);
-      const reversed = reversePlace(x, y, newBoard, turnColor);
-      const getPutted = getPutPlace(reversed, turnColor, passCount);
-      if (passCount === 0) {
-        setTurnColor(3 - turnColor);
-      }
-      setBoard(getPutted);
-    }
+    console.log(x, y);
+    const newBoard = structuredClone(board);
+    minoOpt();
+    const delatedBoard = delateLine(newBoard);
+    setBoard(delatedBoard);
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.setcontainer}>
-        {board.flat().filter((cell) => cell === 3).length === 0
-          ? 'サ終'
-          : turnColor === 1
-            ? '黒'
-            : '白'}
-        <div className={styles.textBold}>
-          siro:{board.flat().filter((cell) => cell === 2).length}
-        </div>
-        <div className={styles.textBold}>
-          kuro:{board.flat().filter((cell) => cell === 1).length}
+      <div className={styles.worldboard}>
+        <div className={styles.board} onClick={() => downCell()}>
+          {board.map((row, y) =>
+            row.map((color, x) => (
+              <div
+                className={styles.cell}
+                key={`${x}-${y}`}
+                onClick={() => {
+                  clickHandler(x, y);
+                }}
+              >
+                {/* I = 1  O = 2  S = 3  Z = 4  L = 5  J = 6  T = 7 */}
+                {color === 1 && <div className={styles.iStone} />}
+                {color === 2 && <div className={styles.oStone} />}
+                {color === 3 && <div className={styles.sStone} />}
+                {color === 4 && <div className={styles.zStone} />}
+                {color === 5 && <div className={styles.lStone} />}
+                {color === 6 && <div className={styles.jStone} />}
+                {color === 7 && <div className={styles.tStone} />}
+              </div>
+            )),
+          )}
         </div>
       </div>
-      <div className={styles.board}>
-        {board.map((row, y) =>
-          row.map((color, x) => (
-            <div
-              className={styles.cell}
-              key={`${x}-${y}`}
-              onClick={() => {
-                clickHandler(x, y);
-              }}
-            >
-              {color !== 0 && color !== 3 && (
-                <div
-                  className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : '#ffff' }}
-                />
-              )}
-              {color === 3 && (
-                <div className={styles.circle} style={{ background: '#rgb(0 200 200)' }} />
-              )}
-            </div>
-          )),
-        )}
+      <div className={styles.informationBoard}>
+        <div className={styles.nextMino}>next</div>
+        <div className={styles.operationBox}>
+          <div className={styles.directionButton} onClick={() => {}}>
+            ←
+          </div>
+          <div className={styles.directionButton} onClick={() => {}}>
+            ↓
+          </div>
+          <div className={styles.directionButton} onClick={() => {}}>
+            →
+          </div>
+          <div className={styles.directionButton} onClick={() => {}}>
+            ↑
+          </div>
+        </div>
       </div>
     </div>
   );
